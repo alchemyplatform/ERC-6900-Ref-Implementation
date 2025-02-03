f9215cd287391597d5bb4aa7784d5a40c18ca258
zer0dot <zer0dot.dev@gmail.com>
2025-02-03 13:10:55 -0500

    feat: block upgrades in 7702 account

--- src/account/SemiModularAccount7702.sol
+++ src/account/SemiModularAccount7702.sol
@@ -6,11 +6,18 @@ import {SemiModularAccount} from "./SemiModularAccount.sol";
 import {IEntryPoint} from "@eth-infinitism/account-abstraction/interfaces/IEntryPoint.sol";
 
 contract SemiModularAccount7702 is SemiModularAccount {
+    error UpgradeNotAllowed();
+
     constructor(IEntryPoint anEntryPoint) SemiModularAccount(anEntryPoint) {}
 
     /// @inheritdoc IModularAccount
     function accountId() external pure virtual override returns (string memory) {
         return "erc6900.reference-semi-modular-account-7702.0.8.0";
+    }
+
+    /// @dev To prevent accidental self-calls, upgrades are disabled in 7702 accounts.
+    function upgradeToAndCall(address, bytes memory) public payable override {
+        revert UpgradeNotAllowed();
     }
 
     /// @dev If the fallback signer is set in storage, ignore the 7702 signer.
